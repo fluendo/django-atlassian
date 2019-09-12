994b7264065eabe89e0cff3cd5ce3c903c041268
Alfonso <aburgool@yahoo.es>
2019-09-12 17:05:12 +0200

    dynamic AutoForm: parse json web fluendo rest response

--- atlassian/forms.py
+++ atlassian/forms.py
@@ -11,13 +11,43 @@ class AutoForm(forms.Form):
                 if k not in self.exclude:
                     if type(v) == int:
                         self.fields[k] = forms.IntegerField()
+                        self.fields[k].widget.attrs['class'] = 'text'
+                    elif type(v) in (type(True), type(False)):
+                        self.fields[k] = forms.BooleanField()
+                        self.fields[k].widget.attrs['class'] = ''
                     else:
                         self.fields[k] = forms.CharField()
+                        self.fields[k].widget.attrs['class'] = 'text medium-long-field'
 
 
 class AccountForm(AutoForm):
     exclude = ['id', 'contact', 'contacts', 'agreements']
-    pass
+    groups = {
+        'info':[
+            'street',
+            'postal_code',
+            'city',
+            'vat',
+            'company_name',
+            'fax',
+            'phone',
+            'web',
+        ],
+        'billing': [
+            'billing_street',
+            'billing_company_name',
+            'billing_country',
+            'billing_postal_code',
+            'billing_city',
+        ],
+        'others': [
+            'fluendo_company',
+            'show_welcome',
+            'num_employees',
+            'enable_send_mails',
+            'user',
+        ],
+    }
 
 
 class ContactForm(forms.Form):
