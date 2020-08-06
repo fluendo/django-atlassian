72c8c1bc0ffea4f9c15b9e31de8ea988cea0eb8b
Jorge Zapata <jorgeluis.zapata@gmail.com>
2020-08-06 11:51:16 +0200

    Enhance the application module's handling
    Now, every app can provide it's own jira_modules.py or
    confluence_modules.py and the system will generate the
    corresponding json description.
    Also, some settings variables are now mandatory in order
    to provide the json description correctly.

--- django_atlassian/views.py
+++ django_atlassian/views.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from importlib import import_module
 import json
 import jwt
 
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.utils.datastructures import MultiValueDictKeyError
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.apps import apps
+from django.views.generic.base import TemplateView
+from django.core.exceptions import ImproperlyConfigured
 
 from models.connect import SecurityContext
 
@@ -49,3 +54,51 @@ def installed(request):
         sc.save()
 
     return HttpResponse(status=204)
+
+
+class ApplicationDescriptor(TemplateView):
+    content_type = 'application/json'
+
+    def get_application_name(self):
+        if self.application_name is None:
+            raise ImproperlyConfigured(
+                "ApplicationDescriptor requires a definition of 'application_name' "
+                "or an implementation of 'get_application_name()'")
+        return self.application_name
+
+    def get_template_names(self):
+        return ['django_atlassian/{}/atlassian-connect.json'.format(self.get_application_name())]
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ApplicationDescriptor, self).get_context_data(*args, **kwargs)
+        base_url = self.request.build_absolute_uri('/')
+        context['base_url'] = getattr(settings, 'URL_BASE', base_url)
+        # Get all the contents of the registered apps application_name_modules.py files
+        modules = {}
+        for app in apps.get_app_configs():
+            try:
+                module = import_module('{}.{}_modules'.format(app.module.__name__, self.get_application_name()))
+                for m in module.modules:
+                    for k,v in m.items():
+                        if not k in modules.keys():
+                            modules[k] = []
+                        modules[k] = modules[k] + v
+            except ImportError:
+                continue
+        context['modules'] = json.dumps(modules)
+        # Get the needed settings or abort
+        context['name'] = getattr(settings, 'DJANGO_ATLASSIAN_{}_NAME'.format(self.get_application_name().upper()))
+        context['description'] = getattr(settings, 'DJANGO_ATLASSIAN_{}_DESCRIPTION'.format(self.get_application_name().upper()))
+        context['key'] = getattr(settings, 'DJANGO_ATLASSIAN_{}_KEY'.format(self.get_application_name().upper()))
+        context['vendor_name'] = getattr(settings, 'DJANGO_ATLASSIAN_VENDOR_NAME')
+        context['vendor_url'] = getattr(settings, 'DJANGO_ATLASSIAN_VENDOR_URL')
+
+        return context
+
+
+class JiraDescriptor(ApplicationDescriptor):
+    application_name = 'jira'
+
+
+class ConfluenceDescriptor(ApplicationDescriptor):
+    application_name = 'confluence'
