2bf93e660dfa426494854392b68f46ca8f6886a5
Jorge Zapata <jorgeluis.zapata@gmail.com>
2020-11-01 12:07:41 +0100

    Add the corresponding models

---
+++ metabase/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django_atlassian.models.connect import SecurityContext
+
+class MetabaseConfiguration(models.Model):
+    """
+    Given that in confluence we can store the metabase configuration
+    as application properties, we need to store them in our own
+    database system
+    """
+    account = models.ForeignKey(SecurityContext)
+    site_url = models.CharField(max_length=256, blank=False, null=False)
+    secret_key = models.CharField(max_length=256, blank=False, null=False)
