55a1fbe11ab25982e8168de0c5da7c25a9eb8fb3
Jorge Zapata <jorgeluis.zapata@gmail.com>
2021-05-23 11:34:59 +0200

    Make celery work in the container
    Update the version of redis.
    Make the requirements be installed on the system path. This is required
    because the celery bin should be accessible.

---
+++ atlassian_addon/celeryapp.py
@@ -0,0 +1,13 @@
+import os
+
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atlassian_addon.settings.dev')
+
+app = Celery('atlassian_addon')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
