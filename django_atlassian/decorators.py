3da15be630ef8972df2b48eb4216aa3e6cba0aff
Jorge Zapata <jorgeluis.zapata@gmail.com>
2021-07-05 23:14:18 +0200

    Add support for context jwt

--- django_atlassian/decorators.py
+++ django_atlassian/decorators.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from functools import wraps
 
-from django.core.exceptions import PermissionDenied
+def jwt_required(view_func):
+    def decorator(*args, **kwargs):
+        return view_func(*args, **kwargs)
+    decorator.jwt_required = True
+    return wraps(view_func)(decorator)
 
-def jwt_required(function):
-    def decorator(request, *args, **kwargs):
-        sc = getattr(request, 'atlassian_sc', None)
-        model = getattr(request, 'atlassian_model', None)
-        if not sc or not model:
-            raise PermissionDenied
-        else:
-            return function(request, *args, **kwargs)
-    return decorator
+def jwt_qsh_exempt(view_func):
+    """Mark a view function as being exempt from the qsh claim protection."""
+    def decorator(*args, **kwargs):
+        return view_func(*args, **kwargs)
+    decorator.jwt_qsh_exempt = True
+    return wraps(view_func)(decorator)
--- django_atlassian/middleware.py
+++ django_atlassian/middleware.py
@@ -3,13 +3,18 @@ from __future__ import unicode_literals
 
 import logging
 import atlassian_jwt
+from atlassian_jwt.url_utils import hash_url, parse_query_params
+
 import threading
+import jwt
+from jwt import DecodeError
 
 from django.utils.deprecation import MiddlewareMixin
 from django.apps import apps
 from django.db import models, connections
 from django.db.utils import ConnectionDoesNotExist 
 
+from django.core.exceptions import PermissionDenied
 from django_atlassian.models.connect import SecurityContext
 from django_atlassian.models.djira import create_model, populate_model
 
@@ -21,9 +26,41 @@ class DjangoAtlassianAuthenticator(atlassian_jwt.Authenticator):
         sc = SecurityContext.objects.filter(client_key=client_key).get()
         return sc.shared_secret
 
+    def claims(self, http_method, url, headers=None, qsh_check_exempt=False):
+        token = self._get_token(
+            headers=headers,
+            query_params=parse_query_params(url))
+
+        claims = jwt.decode(token, verify=False, algorithms=self.algorithms,
+                            options={"verify_signature": False})
+        if not qsh_check_exempt and claims['qsh'] != hash_url(http_method, url):
+            raise DecodeError('qsh does not match')
+
+        # verify shared secret
+        jwt.decode(
+            token,
+            audience=claims.get('aud'),
+            key=self.get_shared_secret(claims['iss']),
+            algorithms=self.algorithms,
+            leeway=self.leeway)
+
+        return claims
+
 
 class JWTAuthenticationMiddleware(MiddlewareMixin):
-    def process_request(self, request):
+    def _create_database(self, name, sc):
+        new_db = {}
+        new_db['id'] = name
+        new_db['ENGINE'] = 'django_atlassian.backends.jira'
+        new_db['NAME'] = sc.host
+        new_db['USER'] = ''
+        new_db['PASSWORD'] = ''
+        new_db['HOST'] = ''
+        new_db['PORT'] = ''
+        new_db['SECURITY'] = sc
+        return new_db
+
+    def _check_jwt(self, request, qsh_check_exempt=False):
         headers = {}
         query = ''
         if request.method == 'POST':
@@ -38,10 +75,22 @@ class JWTAuthenticationMiddleware(MiddlewareMixin):
         uri = request.path
         if query:
             uri = "%s?%s" % (uri, query)
-        try:
-            client_key = auth.authenticate(request.method, uri, headers)
-        except Exception as e:
+
+        claims = auth.claims(request.method, uri, headers, qsh_check_exempt)
+        return claims
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        jwt_required = getattr(view_func, 'jwt_required', False)
+        if not jwt_required:
             return None
+
+        jwt_qsh_exempt = getattr(view_func, 'jwt_qsh_exempt', False)
+
+        try:
+            claims = self._check_jwt(request, qsh_check_exempt=jwt_qsh_exempt)
+            client_key = claims['iss']
+        except Exception as e:
+            raise PermissionDenied
 
         sc = SecurityContext.objects.filter(client_key=client_key).get()
         # Setup the request attributes, the security context and the model
@@ -61,17 +110,5 @@ class JWTAuthenticationMiddleware(MiddlewareMixin):
         request.atlassian_model = model
         request.atlassian_sc = sc
         request.atlassian_db = db
+        request.atlassian_account_id = claims.get('sub')
         return None
-
-
-    def _create_database(self, name, sc):
-        new_db = {}
-        new_db['id'] = name
-        new_db['ENGINE'] = 'django_atlassian.backends.jira'
-        new_db['NAME'] = sc.host
-        new_db['USER'] = ''
-        new_db['PASSWORD'] = ''
-        new_db['HOST'] = ''
-        new_db['PORT'] = ''
-        new_db['SECURITY'] = sc
-        return new_db
