0380244af111d13175e624080f209de731d58f45
Alfonso Burgos <aburgos@fluendo.com>
2022-06-08 10:24:17 +0200

    fixup! Refactor application: add supervisor to local dev

--- supervisor/app.sh
+++ supervisor/app.sh
@@ -3,7 +3,8 @@
 cd /app/src/
 
 poetry install
-poetry run python manage.py collectstatic
-poetry run python manage.py migrate
+poetry run python manage.py collectstatic --noinput
+poetry run python manage.py migrate --noinput
+supervisorctl start celery-worker
 poetry run uwsgi --master --http 0.0.0.0:8000 \
 --module atlassian_addon.wsgi:application
